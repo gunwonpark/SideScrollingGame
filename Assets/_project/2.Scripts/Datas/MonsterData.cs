5fdf6088e202bafab80b52d085ca38174569613e
gunwonpark <parkgunwon1@naver.com>
2024-08-07 10:42:45 +0900

    프로젝트 Init
    기본 스크립트 및 이미지 파일 저장

---
+++ Assets/_project/2.Scripts/Datas/MonsterData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class MonsterData : MonoBehaviour
+{
+    public void Init()
+    {
+
+    }
+}
---
+++ Assets/_project/2.Scripts/Monster/Monster.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public void Init()
+    {
+
+    }
+}
